66c9ab7856f3fd5cd42b5bf2e3fdb40918d5435c
Julien Morot <jmorot@gmail.com>
2019-05-22 18:25:38 +0200

    Switch to chrony NTP server

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -1,43 +1,27 @@
 class basesystem::ntp {
-    $service_name = 'ntp'
-    $packages = [ 'ntp']
-    package { $packages : ensure => installed }
+    $service_name = 'chrony'
+    $packages = [ 'ntp' ]
+    package { $packages : ensure => purged }
+    package { "chrony" : ensure => installed }
 
-    file { 'ntp.conf':
+    File { 'ntp.conf':
         path    => '/etc/ntp.conf',
-        ensure  => file,
-        mode    => '644',
-        owner   => 'root',
-        group   => 'root',
-        require => Package['ntp'],
-        source  => "puppet:///modules/${module_name}/ntp.conf",
-        notify  => Service["ntp"],
+        ensure  => absent,
     }
 
-    file { 'leap-seconds.list':
+    File { 'leap-seconds.list':
         path    => '/var/lib/ntp/leap-seconds.list',
-        ensure  => file,
-        mode    => '644',
-        owner   => 'root',
-        group   => 'root',
-        require => Package['ntp'],
-        source  => "puppet:///modules/${module_name}/leap-seconds.list",
-        notify  => Service["ntp"],
+        ensure  => absent,
     }
 
-	file_line { 'apparmor_leap-seconds.list':
-  		ensure => present,
-  		path   => '/etc/apparmor.d/usr.sbin.ntpd',
-  		line   => "  /var/lib/ntp/leap-seconds.list r,",
-  		after  => "  /var/lib/ntp/\*drift rw,",
-	}
-
-    service { $service_name :
+    Service { $service_name :
         name      => $service_name,
         ensure    => running,
         enable    => true,
         hasrestart => true,
         hasstatus  => true,
     }
+
+
 }
 
