9b5e51ef7a99cae050e7a04ff61e28c312f58e9b
Julien Morot <jmorot@gmail.com>
2019-05-22 18:31:00 +0200

    Ensure nscd is running

--- manifests/nscd.pp
+++ manifests/nscd.pp
@@ -1,4 +1,15 @@
 class basesystem::nscd {
-    package { "nscd": ensure => installed }
+    $service_name = 'nscd'
+
+    Package { "nscd": ensure => installed }
+
+    Service { $service_name :
+        name      => $service_name,
+        ensure    => running,
+        enable    => true,
+        hasrestart => true,
+        hasstatus  => true,
+    }
+
 }
 
